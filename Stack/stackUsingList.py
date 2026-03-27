b92c4b186ca26c086667c33bdafe2953c64968cc
Vikram Markali <vikrammarkali116@gmail.com>
2026-03-27 18:29:02 -0400

    stack using list...

---
+++ Stack/stackUsingList.py
@@ -0,0 +1,16 @@
+# this code shows stack in python using list...
+
+stack = list()
+
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print(stack)
+
+# pop stack elements...
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print(stack)
